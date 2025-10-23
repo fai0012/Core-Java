c83a8876056f6d385ae53ef9aa5cf116c66e990d
Fai Shaikh <fai.shaikh@gmail.com>
2025-10-23 11:52:15 +0530

    array

---
+++ src/conversionDatatypes/Explicitcasting.java
@@ -0,0 +1,14 @@
+package conversionDatatypes;
+
+public class Explicitcasting {
+public static void main(String[]args) {
+	double a = 50122.502525252525;
+	System.out.println("double Representation:"+a);
+	float f = (float)a;
+	System.out.println("float Representation:"+f);
+	long b = (long)a;
+	System.out.println("long Representaion:"+b);
+	int c = (int)b;
+	System.out.println("int Representation:"+c);
+}
+}
---
+++ src/conversionDatatypes/ImplicitDataTypes.java
@@ -0,0 +1,13 @@
+package conversionDatatypes;
+
+public class ImplicitDataTypes {
+
+	public static void main(String[]args) {
+		int a=100;
+		System.out.println("Representaion of int:"+a);
+		long b = a;
+		System.out.println("Representation of long:"+b);
+		float c = b;
+		System.out.println("Representation of float1:"+ c);
+		} 
+}
