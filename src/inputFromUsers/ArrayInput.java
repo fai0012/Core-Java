f8a5e132685a98b9016c96ea446e7c6f689f444a
fai0012 <shaikhfai798@gmail.com>
2025-11-05 03:57:08 +0530

    InputFromUsers

---
+++ src/inputFromUsers/ArrayInput.java
@@ -0,0 +1,24 @@
+package inputFromUsers;
+
+import java.util.Scanner;
+
+public class ArrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("You entered:");
+        for(int n : arr) {
+            System.out.println(n);
+ sc.close();      
+        }
+    }
+}
+
