b9309e1997baca1070868c213186ec2718c24bc6
fai0012 <shaikhfai798@gmail.com>
2025-11-05 03:55:47 +0530

    InputFromUsers

---
+++ src/inputFromUsers/MultipleInputs.java
@@ -0,0 +1,19 @@
+package inputFromUsers;
+
+import java.util.Scanner;
+
+public class MultipleInputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+ sc.close();
+    }
+}
