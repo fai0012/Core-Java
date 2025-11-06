4b3ca6966654f1dc6e2222c2dc3040439109bae9
fai0012 <shaikhfai798@gmail.com>
2025-11-06 14:48:55 +0530

    control_flowStatement

---
+++ src/control_flowStatement/Print1to10.java
@@ -0,0 +1,11 @@
+package control_flowStatement;
+
+public class Print1to10 {
+	
+	    public static void main(String[] args) {
+	        for (int i = 1; i <= 10; i++) { // using for loop
+	            System.out.println(i);
+	        }
+	    }
+	}
+
